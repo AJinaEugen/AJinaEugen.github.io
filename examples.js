901c33b56b14d8f545cb1b339742c92a811968fb
AJinaEugen <alexandru.jina@gmail.com>
2023-06-14 20:35:25 +0300

    PLayed with some js variables

---
+++ examples.js
@@ -0,0 +1,19 @@
+var employed = false; // boolean
+var age = 24; // number
+var name = 'First Last'; // string
+
+// object (array)
+var skills = ['html', 'css', 'js'];
+// object (json)
+var person = {
+  employed: true,
+  age: 29
+};
+// object (DOM, other)
+var el = document.getElementById('el');
+
+function getMsg(name){
+    return  `Hey ${name}  ! Whassap` // Backtick usage
+
+}
+
