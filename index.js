2938abd0741e2f81b9469396cbc9cc8e873fff9c
AJinaEugen <alexandru.jina@gmail.com>
2023-06-21 20:53:31 +0300

    Worked on the adding an active class to the different menu elements to make it look like their enabled

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ var activePage = "welcome-text";
 //utility functions
 function hide(id) {
   document.getElementById(id).style.display = "none";
+  let link = document.querySelector(`#top-menu-bar [data-page=${id}]`);
+  console.log(link);
+  link.classList.remove("active");
 }
 
 function showPage(id) {
+  let link = document.querySelector(`#top-menu-bar [data-page=${id}]`);
+  console.log(link);
+  link.classList.add("active");
+
   hide(activePage);
   var page = document.getElementById(id);
   page.style.display = "block";
@@ -21,6 +28,7 @@ function clickOnMenu(e) {
     "target.matches returns true or false depending if the user clicks on the coorect element",
     e.target.matches("a")
   );
+  console.log(e.target.id);
 
   var link = e.target.closest("a");
 
