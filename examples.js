df82db0e64cf9ac590345fa16c3ff66fbc6b6ae9
AJinaEugen <alexandru.jina@gmail.com>
2023-06-28 18:21:08 +0300

    added arrow function

--- examples.js
+++ examples.js
@@ -1,19 +1,21 @@
 var employed = false; // boolean
 var age = 24; // number
-var name = 'First Last'; // string
+var name = "First Last"; // string
 
 // object (array)
-var skills = ['html', 'css', 'js'];
+var skills = ["html", "css", "js"];
 // object (json)
 var person = {
   employed: true,
-  age: 29
+  age: 29,
 };
 // object (DOM, other)
-var el = document.getElementById('el');
+var el = document.getElementById("el");
 
-function getMsg(name){
-    return  `Hey ${name}  ! Whassap` // Backtick usage
-
+function getMsg(name) {
+  return `Hey ${name}  ! Whassap`; // Backtick usage
 }
 
+var fvariable = function () {
+  console.info("functie ca varaiabila");
+};
